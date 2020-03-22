e9e4d7e2840a8b58b4649c4d6e45df32803aaec7
je.berdugo10 <je.berdugo10@uniandes.edu.co>
2020-03-22 01:30:27 -0500

    Funcionamiento persistencia

---
+++ src/main/java/uniandes/isis2304/alohandes/negocio/AlohAndes.java
@@ -0,0 +1,98 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad	de	los	Andes	(Bogotá	- Colombia)
+ * Departamento	de	Ingeniería	de	Sistemas	y	Computación
+ * Licenciado	bajo	el	esquema	Academic Free License versión 2.1
+ * 		
+ * Curso: isis2304 - Sistemas Transaccionales
+ * Proyecto: AlohAndes Uniandes
+ * @version 1.0
+ * @author Germán Bravo
+ * Julio de 2018
+ * 
+ * Revisado por: Claudia Jiménez, Christian Ariza
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+package uniandes.isis2304.alohandes.negocio;
+
+import java.sql.Timestamp;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import com.google.gson.JsonObject;
+
+import uniandes.isis2304.alohandes.persistencia.PersistenciaAlohAndes;
+
+
+/**
+ * Clase principal del negocio
+ * Sarisface todos los requerimientos funcionales del negocio
+ *
+ * @author Germán Bravo
+ */
+public class AlohAndes 
+{
+	/* ****************************************************************
+	 * 			Constantes
+	 *****************************************************************/
+	/**
+	 * Logger para escribir la traza de la ejecución
+	 */
+	private static Logger log = Logger.getLogger(AlohAndes.class.getName());
+	
+	/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
+	/**
+	 * El manejador de persistencia
+	 */
+	private PersistenciaAlohAndes pp;
+	
+	/* ****************************************************************
+	 * 			Métodos
+	 *****************************************************************/
+	/**
+	 * El constructor por defecto
+	 */
+	public AlohAndes ()
+	{
+		pp = PersistenciaAlohAndes.getInstance ();
+	}
+	
+	/**
+	 * El constructor qye recibe los nombres de las tablas en tableConfig
+	 * @param tableConfig - Objeto Json con los nombres de las tablas y de la unidad de persistencia
+	 */
+	public AlohAndes (JsonObject tableConfig)
+	{
+		pp = PersistenciaAlohAndes.getInstance (tableConfig);
+	}
+	
+	/**
+	 * Cierra la conexión con la base de datos (Unidad de persistencia)
+	 */
+	public void cerrarUnidadPersistencia ()
+	{
+		pp.cerrarUnidadPersistencia ();
+	}
+	
+	
+	
+	/* ****************************************************************
+	 * 			Métodos para administración
+	 *****************************************************************/
+
+	/**
+	 * Elimina todas las tuplas de todas las tablas de la base de datos de AlohAndes
+	 * @return Un arreglo con 7 números que indican el número de tuplas borradas en las tablas GUSTAN, SIRVEN, VISITAN, BEBIDA,
+	 * TIPOBEBIDA, BEBEDOR y BAR, respectivamente
+	 */
+	public long [] limpiarAlohAndes ()
+	{
+        log.info ("Limpiando la BD de AlohAndes");
+        long [] borrrados = pp.limpiarAlohAndes();	
+        log.info ("Limpiando la BD de AlohAndes: Listo!");
+        return borrrados;
+	}
+}
---
+++ src/main/java/uniandes/isis2304/alohandes/persistencia/PersistenciaAlohAndes.java
@@ -0,0 +1,310 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad	de	los	Andes	(Bogotá	- Colombia)
+ * Departamento	de	Ingeniería	de	Sistemas	y	Computación
+ * Licenciado	bajo	el	esquema	Academic Free License versión 2.1
+ * 		
+ * Curso: isis2304 - Sistemas Transaccionales
+ * Proyecto: AlohAndes Uniandes
+ * @version 1.0
+ * @author Germán Bravo
+ * Julio de 2018
+ * 
+ * Revisado por: Claudia Jiménez, Christian Ariza
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+package uniandes.isis2304.alohandes.persistencia;
+
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.jdo.JDODataStoreException;
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Transaction;
+
+import org.apache.log4j.Logger;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+
+/**
+ * Clase para el manejador de persistencia del proyecto AlohAndes
+ * Traduce la información entre objetos Java y tuplas de la base de datos, en ambos sentidos
+ * Sigue un patrón SINGLETON (Sólo puede haber UN objeto de esta clase) para comunicarse de manera correcta
+ * con la base de datos
+ * Se apoya en las clases SQLBar, SQLBebedor, SQLBebida, SQLGustan, SQLSirven, SQLTipoBebida y SQLVisitan, que son 
+ * las que realizan el acceso a la base de datos
+ * 
+ * @author Germán Bravo
+ */
+public class PersistenciaAlohAndes 
+{
+	/* ****************************************************************
+	 * 			Constantes
+	 *****************************************************************/
+	/**
+	 * Logger para escribir la traza de la ejecución
+	 */
+	private static Logger log = Logger.getLogger(PersistenciaAlohAndes.class.getName());
+	
+	/**
+	 * Cadena para indicar el tipo de sentencias que se va a utilizar en una consulta
+	 */
+	public final static String SQL = "javax.jdo.query.SQL";
+
+	/* ****************************************************************
+	 * 			Atributos
+	 *****************************************************************/
+	/**
+	 * Atributo privado que es el único objeto de la clase - Patrón SINGLETON
+	 */
+	private static PersistenciaAlohAndes instance;
+	
+	/**
+	 * Fábrica de Manejadores de persistencia, para el manejo correcto de las transacciones
+	 */
+	private PersistenceManagerFactory pmf;
+	
+	/**
+	 * Arreglo de cadenas con los nombres de las tablas de la base de datos, en su orden:
+	 * Secuenciador, tipoBebida, bebida, bar, bebedor, gustan, sirven y visitan
+	 */
+	private List <String> tablas;
+	
+	/**
+	 * Atributo para el acceso a las sentencias SQL propias a PersistenciaAlohAndes
+	 */
+	private SQLUtil sqlUtil;
+	
+	
+	
+	/* ****************************************************************
+	 * 			Métodos del MANEJADOR DE PERSISTENCIA
+	 *****************************************************************/
+
+	/**
+	 * Constructor privado con valores por defecto - Patrón SINGLETON
+	 */
+	private PersistenciaAlohAndes ()
+	{
+		pmf = JDOHelper.getPersistenceManagerFactory("AlohAndes");		
+		crearClasesSQL ();
+		
+		// Define los nombres por defecto de las tablas de la base de datos
+		tablas = new LinkedList<String> ();
+		tablas.add ("AlohAndes_sequence");
+		tablas.add ("USUARIO");
+		tablas.add ("OPERADOR");
+		tablas.add ("CLIENTE");
+		tablas.add ("RESERVA");
+		tablas.add ("OFERTA");
+		tablas.add ("ALOJAMIENTO");
+		
+}
+
+	/**
+	 * Constructor privado, que recibe los nombres de las tablas en un objeto Json - Patrón SINGLETON
+	 * @param tableConfig - Objeto Json que contiene los nombres de las tablas y de la unidad de persistencia a manejar
+	 */
+	private PersistenciaAlohAndes (JsonObject tableConfig)
+	{
+		crearClasesSQL ();
+		tablas = leerNombresTablas (tableConfig);
+		
+		String unidadPersistencia = tableConfig.get ("unidadPersistencia").getAsString ();
+		log.info ("Accediendo unidad de persistencia: " + unidadPersistencia);
+		pmf = JDOHelper.getPersistenceManagerFactory (unidadPersistencia);
+	}
+
+	/**
+	 * @return Retorna el único objeto PersistenciaAlohAndes existente - Patrón SINGLETON
+	 */
+	public static PersistenciaAlohAndes getInstance ()
+	{
+		if (instance == null)
+		{
+			instance = new PersistenciaAlohAndes ();
+		}
+		return instance;
+	}
+	
+	/**
+	 * Constructor que toma los nombres de las tablas de la base de datos del objeto tableConfig
+	 * @param tableConfig - El objeto JSON con los nombres de las tablas
+	 * @return Retorna el único objeto PersistenciaAlohAndes existente - Patrón SINGLETON
+	 */
+	public static PersistenciaAlohAndes getInstance (JsonObject tableConfig)
+	{
+		if (instance == null)
+		{
+			instance = new PersistenciaAlohAndes (tableConfig);
+		}
+		return instance;
+	}
+
+	/**
+	 * Cierra la conexión con la base de datos
+	 */
+	public void cerrarUnidadPersistencia ()
+	{
+		pmf.close ();
+		instance = null;
+	}
+	
+	/**
+	 * Genera una lista con los nombres de las tablas de la base de datos
+	 * @param tableConfig - El objeto Json con los nombres de las tablas
+	 * @return La lista con los nombres del secuenciador y de las tablas
+	 */
+	private List <String> leerNombresTablas (JsonObject tableConfig)
+	{
+		JsonArray nombres = tableConfig.getAsJsonArray("tablas") ;
+
+		List <String> resp = new LinkedList <String> ();
+		for (JsonElement nom : nombres)
+		{
+			resp.add (nom.getAsString ());
+		}
+		
+		return resp;
+	}
+	
+	/**
+	 * Crea los atributos de clases de apoyo SQL
+	 */
+	private void crearClasesSQL ()
+	{
+				
+		sqlUtil = new SQLUtil(this);
+	}
+
+	/**
+	 * @return La cadena de caracteres con el nombre del secuenciador de parranderos
+	 */
+	public String darSeqAlohAndes ()
+	{
+		return tablas.get (0);
+	}
+
+	/**
+	 * @return La cadena de caracteres con el nombre de la tabla de TipoBebida de parranderos
+	 */
+	public String darTablaUsuario ()
+	{
+		return tablas.get (1);
+	}
+
+	/**
+	 * @return La cadena de caracteres con el nombre de la tabla de Bebida de parranderos
+	 */
+	public String darTablaOperador ()
+	{
+		return tablas.get (2);
+	}
+
+	/**
+	 * @return La cadena de caracteres con el nombre de la tabla de Bar de parranderos
+	 */
+	public String darTablaCliente ()
+	{
+		return tablas.get (3);
+	}
+
+	/**
+	 * @return La cadena de caracteres con el nombre de la tabla de Bebedor de parranderos
+	 */
+	public String darTablaReserva()
+	{
+		return tablas.get (4);
+	}
+
+	/**
+	 * @return La cadena de caracteres con el nombre de la tabla de Gustan de parranderos
+	 */
+	public String darTablaOferta ()
+	{
+		return tablas.get (5);
+	}
+
+	/**
+	 * @return La cadena de caracteres con el nombre de la tabla de Sirven de parranderos
+	 */
+	public String darTablaAlojamiento ()
+	{
+		return tablas.get (6);
+	}
+
+	
+	
+	/**
+	 * Transacción para el generador de secuencia de AlohAndes
+	 * Adiciona entradas al log de la aplicación
+	 * @return El siguiente número del secuenciador de AlohAndes
+	 */
+	private long nextval ()
+	{
+        long resp = sqlUtil.nextval (pmf.getPersistenceManager());
+        log.trace ("Generando secuencia: " + resp);
+        return resp;
+    }
+	
+	/**
+	 * Extrae el mensaje de la exception JDODataStoreException embebido en la Exception e, que da el detalle específico del problema encontrado
+	 * @param e - La excepción que ocurrio
+	 * @return El mensaje de la excepción JDO
+	 */
+	private String darDetalleException(Exception e) 
+	{
+		String resp = "";
+		if (e.getClass().getName().equals("javax.jdo.JDODataStoreException"))
+		{
+			JDODataStoreException je = (javax.jdo.JDODataStoreException) e;
+			return je.getNestedExceptions() [0].getMessage();
+		}
+		return resp;
+	}
+
+
+	/**
+	 * Elimina todas las tuplas de todas las tablas de la base de datos de AlohAndes
+	 * Crea y ejecuta las sentencias SQL para cada tabla de la base de datos - EL ORDEN ES IMPORTANTE 
+	 * @return Un arreglo con 7 números que indican el número de tuplas borradas en las tablas GUSTAN, SIRVEN, VISITAN, BEBIDA,
+	 * TIPOBEBIDA, BEBEDOR y BAR, respectivamente
+	 */
+	public long [] limpiarAlohAndes ()
+	{
+		PersistenceManager pm = pmf.getPersistenceManager();
+        Transaction tx=pm.currentTransaction();
+        try
+        {
+            tx.begin();
+            long [] resp = sqlUtil.limpiarAlohAndes (pm);
+            tx.commit ();
+            log.info ("Borrada la base de datos");
+            return resp;
+        }
+        catch (Exception e)
+        {
+//        	e.printStackTrace();
+        	log.error ("Exception : " + e.getMessage() + "\n" + darDetalleException(e));
+        	return new long[] {-1, -1, -1, -1, -1, -1, -1};
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            pm.close();
+        }
+		
+	}
+	
+
+ }
